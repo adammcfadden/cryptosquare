1919bb8ee5a9b026a7657caa48ac8a5f2ca3e4e3
Guest User <student@epicodus.com>
2015-05-26 16:15:05 -0700

    cryptograph function is working, but only for a 4 column input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,58 @@
+// var cryptograph = function(sentence) {
+//   sentence = sentence.replace(/\W+/g, '').toLowerCase();
+//   column_length = sentence.length()
+//   return sentence
+// };
+//
+
 var cryptograph = function(sentence) {
-  sentence = sentence.replace(/\W+/g, '').toLowerCase();
-  return sentence
+  var sentence = sentence.replace(/\W+/g, '').toLowerCase();
+  var coded_array = [];
+  var len = sentence.length
+  var column_length = Math.ceil(Math.sqrt(len));
+  var sentence_array = sentence.split("");
+  for (; len > 0 ; len -= column_length) {
+    var row = sentence_array.splice(0,column_length).join("");   //[a, e, e, s, s, f, g, g, i]
+    coded_array.push(row);
+  };
+  len = sentence.length
+  var word_array = []
+  var word1 = coded_array.shift()
+  var word2 = coded_array.shift()
+  var word3 = coded_array.shift()
+  for (; len > 0; len -= 3) {
+    word_array.push(word1.split("").splice(0,1))
+    word1 = word1.split("").slice(1).join("")
+    word_array.push(word2.split("").splice(0,1))
+    word2 = word2.split("").slice(1).join("")
+    word_array.push(word3.split("").splice(0,1))
+    word3 = word3.split("").slice(1).join("")
+  }
+  // return word1
+  return word_array.join("");
 };
+
+
+
+  // while (sentence_array.length > 0) {
+  //   var row = sentence_array.splice(0,1,2,3).join("");   //[a, e, e, s, s, f, g, g, i]
+  //   coded_array.push(row);
+  // };
+
+
+
+
+//
+//
+//
+//
+// Math.ceil(Math.sqrt(x))
+//
+//
+// array = [ [sour],[four]
+//           [hour],[tour]
+//                         ]
+//
+//   until array.length == 0 {
+//     new_array
+//   }
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,14 @@
 describe('cryptograph', function(){
 
   it("will remove all whitespace, and punctuation in a string.", function(){
-    expect(cryptograph("the dog's ball goes for a toss.")).to.equal("thedogsballgoesforatoss");
+    expect(cryptograph("the dogs ball goes for a toss")).to.equal("thedogsballgoesforatoss");
   });
 
   it("will downcase all words in the sentence", function() {
-    expect(cryptograph("The Dog Is Good")).to.equal("thedogisgood")
+    expect(cryptograph("The Dog Is Good")).to.equal("thedogisgood");
+  });
+
+  it("will output the cryptosquare readout of the input sentence", function() {
+    expect(cryptograph("I've got an Idea")).to.equal("ioivt deaeg na");
   });
 });
